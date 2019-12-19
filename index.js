07533d3eacff3a22a65670f9c15c176773c18164
Hugo Carvalho <hugosc@gmail.com>
2019-12-19 20:48:35 -0300

    fixing

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const dbConnection = sqlite.open(path.resolve(__dirname, 'banco.sqlite'), { Prom
 const port = process.env.PORT || 
 
 
-app.set('views', path.joun(__dirname,'views'))
+app.set('views', path.join(__dirname,'views'))
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true}))
