5867b4326200bc57c6f2c11990caee59b74e6b32
Hugo Carvalho <hugosc@gmail.com>
2019-12-20 16:55:35 -0300

    FIXING PUBLIC

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 3000
 app.set('views', path.join(__dirname,'views'))
 app.set('view engine', 'ejs')
 app.engine('ejs', require('ejs').__express)
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname,'public')))
 app.use(bodyParser.urlencoded({ extended: true}))
 
 app.get('/', async(request, response) => {
