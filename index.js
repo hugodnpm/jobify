5b93279b0a1a4a26195b2ee9d32ad553b42cb007
Hugo Carvalho <hugosc@gmail.com>
2019-12-19 21:12:45 -0300

    tentando fazer rodar zeit

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ const path = require('path')
 const sqlite = require('sqlite')
 const dbConnection = sqlite.open(path.resolve(__dirname, 'banco.sqlite'), { Promise })
 
-const port = process.env.PORT || 
-
+const port = process.env.PORT || 3000
 
 app.set('views', path.join(__dirname,'views'))
 app.set('view engine', 'ejs')
